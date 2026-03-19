6d9a27c8b186b94121375f4a7803a8f059780b52
Rajaseelan Nadar <rajaseelan.nadar@bom2ad.gupshup.io>
2026-03-19 17:45:56 +0530

    Updated project files and Docker config

---
+++ debug.php
@@ -0,0 +1,6 @@
+<?php
+
+echo "HOST: " . getenv('MYSQLHOST') . "<br>";
+echo "PORT: " . getenv('MYSQLPORT') . "<br>";
+echo "DB: " . getenv('MYSQLDATABASE') . "<br>";
+echo "USER: " . getenv('MYSQLUSER') . "<br>";
